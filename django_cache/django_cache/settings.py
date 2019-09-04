6822c2bdcc76c050cd088949302c5620a743ce85
JDSalisbury <Salisbury.jeffery@gmail.com>
2019-09-04 11:35:15 -0400

    Add apps to django project

--- django_cache/django_cache/settings.py
+++ django_cache/django_cache/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'testappmodel',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
