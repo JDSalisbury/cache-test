2b6ff59ce40f00bf408ed225bfc2b62534fc687b
JDSalisbury <Salisbury.jeffery@gmail.com>
2019-09-04 15:30:29 -0400

    Setting up Docker.

--- django_cache/django_cache/settings.py
+++ django_cache/django_cache/settings.py
@@ -75,10 +75,20 @@ WSGI_APPLICATION = 'django_cache.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'CachTime',
+        'USER': 'root',
+        'PASSWORD': 'password',
+        'HOST': 'localhost'
     }
 }
 
